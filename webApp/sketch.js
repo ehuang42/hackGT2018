76c862268d2a73717d3d523f72670950d4ec402a
alexa <alexaflesch@gmail.com>
2018-10-21 01:49:30 -0400

    Updated UI

--- webApp/sketch.js
+++ webApp/sketch.js
@@ -68,44 +68,68 @@ function upload() {
           b2 = colors[2].color.blue;
 
           document.getElementById("dominant_id").style.backgroundColor = 'rgb(' + [r0,g0,b0].join(',') + ')';
+          document.getElementById("dominant_id").innerHTML = "Primary Color: " + rgbToHex(r0,g0,b0);
+          document.getElementById('dominant_id').style.fontFamily = "Chakra Petch, sans-serif";
           document.getElementById("secondary_id").style.backgroundColor = 'rgb(' + [r1,g1,b1].join(',') + ')';
+          document.getElementById("secondary_id").innerHTML = "Secondary Color: " + rgbToHex(r1,g1,b1);
+          document.getElementById('secondary_id').style.fontFamily = "Chakra Petch, sans-serif";
           document.getElementById("tertiary_id").style.backgroundColor = 'rgb(' + [r2,g2,b2].join(',') + ')';
+          document.getElementById("tertiary_id").innerHTML = "Tertiary Color: " + rgbToHex(r2,g2,b2);
+          document.getElementById('tertiary_id').style.fontFamily = "Chakra Petch, sans-serif";
+          document.getElementById('tertiary_id').style.marginTop = "0px";
+          console.log(r0 + g0 + b0);
 
           if (r0 < 40 && g0 < 40 && b0 < 40) {
               console.log("black");
               document.body.style.backgroundColor = '#424747';
               document.getElementById("h2").style.backgroundColor = '#fbfcfc';
               document.getElementById("h2").style.color = "white";
+              document.getElementById("h1").style.color = "white";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "Metal";
-          } else if (r0 > 180 && g0 > 180 && b0 > 180) {
+          } else if (r0 > 100 && g0 > 100 && b0 > 100) {
               console.log("white");
               document.body.style.backgroundColor = '#eaeaea';
+              document.getElementById("h1").style.color = "black";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "Opera";
-          } else if (r0 > 100 && g0 > 100 && b0 > 100) {
+          } else if (r0 > 60 && g0 > 60 && b0 > 60) {
               console.log("grey");
               document.body.style.backgroundColor = '#c0bfc0';
               document.getElementById("h2").style.backgroundColor = '#f8fafa';
+              document.getElementById("h1").style.color = "black";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "HipHop";
           } else if (r0 > 100 && g0 > 100 && b0 < 65) {
               console.log("yellow");
               document.body.style.backgroundColor = '#fcaf6d';
               document.getElementById('h2').style.backgroundColor = '#fe8500';
+              document.getElementById("h1").style.color = "black";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "Pop";
-          } else if (r0 > 50 && g0 > 15 && b0 > 15) {
+          } else if (r0 > 30 && g0 > 15 && b0 > 15 && r0 > b0) {
               console.log("brown");
               document.body.style.backgroundColor = '#3a201b';
+              document.getElementById("h1").style.color = "white";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "Folk";
           } else if (r0 > g0 && r0 > b0) {
               console.log("red");
               document.body.style.backgroundColor = "red";
+              document.getElementById("h1").style.color = "black";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "Love";
           } else if (g0 > r0 && g0 > b0) {
               console.log("green");
               document.body.style.backgroundColor = "green";
+              document.getElementById("h1").style.color = "black";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "HipHop";
           } else {
               console.log("blue");
-              document.body.style.backgroundColor = "blue";
+              document.body.style.backgroundColor = "#3d76d3";
+              document.getElementById("h1").style.color = "black";
+              document.getElementById('h1').style.fontFamily = "Chakra Petch, sans-serif";
               resultStr = "Sad";
           }
           var spotPlaylist = "";
@@ -146,6 +170,14 @@ function upload() {
       }
     })
   })
+}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 function draw() {
