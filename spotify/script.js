2fecb77a087cfd134e972fdefb458f9b889f3be2
mahimachander <mchander6@gatech.edu>
2018-10-20 20:00:52 -0400

    spotify links added

--- spotify/script.js
+++ spotify/script.js
@@ -7,8 +7,7 @@
     play track
 */
 
-//require spotify api for javascript
-//instantiate
+
 var Spotify = require('spotify-web-api-js');
 var s = new Spotify();
 
@@ -16,16 +15,48 @@ var s = new Spotify();
 var spotifyApi = new SpotifyWebApi();
 
 //request access token to utilize Spotify api
+spotifyApi.setAccessToken('BQBOpc05Dh3jvVZG9aBQ8U6koV0fAt8F4XVbonODDCuQa-q6qu7tVW5yjZm9ZxBfhvX69S4bvZU-3LcCbPmqNqe_B87MhaHB3exGubs6g82JmhxeU6jSd9GQt43gGskyMFkg-Di9HMZxduzpD1kKLKUAuql3rUY2r9GZvQ&refresh_token=AQC5ZdvjwRkTCHGy0vA1NmmFBC4H6ToSDLsNB1ibcZcH4svq68j6878gG0h4HWPVo_yIwfsp-STZjudo17ijakNu8Ty3bLCE7plDX8eGxVx0qbScFyJ0GfJ5QWJoTlpAhpVuIg');
 
-spotifyApi.setAccessToken('BQDQxohKtefRHe3ZFTpWnX4OEMWQDr84VdzOzSKbLA_3LHyd-4R46BhaxdnBxjcM5iVdnlRtEb_7NI5G1md4cAHeIzFrAwH4EbEZoBNEq7sWSN8ovugeiOaWX5qw1Ie0P3fZ-xfF0uFkvTJEqnUAs_iRKHwnF87ABtb94A&refresh_token=AQBWPuRFd6UaZsX787ToINcM1-wC8GqkvWPDCNOP135pK6OdhQERBY6WbWrCLXmaQOnNi4n3T9-FUjZx7TWNuMj7uM0DKmdoGKyZljAThNyaNcMc8lnG2My7pimt3ScWRCA7lw');
-
+//api methods return promise objects
 spotifyApi.setPromiseImplementation(Q);
 
-var searchTerm = "red"
+var resulturl = "red"
 
-var playlist = spotifyApi.searchPlaylists('playlist:' + searchTerm)
+//matchPlaylist is a promise object
+//On success: the response body contains an array of artist objects, simplified
+// album objects, and/or track objects wrapped in a paging object in JSON.
+//https://developer.spotify.com/documentation/web-api/reference/search/search/
+/*
+var matchPlaylist = spotifyApi.searchPlaylists('playlist:' + searchTerm)
   .then(function(data) {
-    console.log('Search tracks by "Love" in the artist name', data);
+    console.log('Success: search tracks by "Love" in the artist name');
+    var playlist = data;
   }, function(err) {
     console.error(err);
   });
+*/
+
+//put result string into an array
+var songinArray = ----;
+
+
+songInput = {
+  "uris": songinArray,
+  "offset": {
+    "position": 0
+  },
+  "position_ms": 0
+}
+
+var deviceobj = spotifyApi.getMyDevices(function(console.log('Got DeviceID'))
+
+var deviceId = deviceobj['devices'][0]['id']
+
+spotifyApi.play(songinArray, function(console.log('Song played')))
+
+
+
+
+
+
+
